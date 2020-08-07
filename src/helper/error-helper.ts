ea7c5953a90bd0c2dd189f2e5b7ed9ee319e2906
Portu89 <elportu89@gmail.com>
2020-08-07 17:09:54 +0200

    remove files

--- src/helper/error-helper.ts
+++
@@ -1,6 +0,0 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-
-export function handleError(err: HttpErrorResponse): Observable<never> {
-    return throwError(err);
-}
